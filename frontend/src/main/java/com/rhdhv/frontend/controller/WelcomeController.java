acb30b6ea9e73fe1045afdb0e56802890f08c79a
Bernardo Garcia <bernardo.garcia.loaiza@sentia.com>
2020-08-18 18:59:48 +0200

    Creating Client java class as an entity for name and id

--- frontend/src/main/java/com/rhdhv/frontend/controller/WelcomeController.java
+++ frontend/src/main/java/com/rhdhv/frontend/controller/WelcomeController.java
@@ -1,37 +1,52 @@
 package com.rhdhv.frontend.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rhdhv.demo.entity.Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class WelcomeController {
+    /*Options dropdown for html actions */
+    List<String> listAction = Arrays.asList("Add", "Delete");
     // inject via application.properties
     @Value("${welcome.message}")
     private String message;
 
-    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
-
     @GetMapping("/")
     public String main(Model model) {
         model.addAttribute("message", message);
-        model.addAttribute("tasks", tasks);
-
+        Client client = new Client();
+        model.addAttribute("client", client);
+        model.addAttribute("listAction", listAction);
+        /* Getting User list clients from backend service */
+        List<Client> userList = getClients();
+        model.addAttribute("userList", userList);
         return "welcome"; //view
     }
 
-    // /hello?name=kotlin
+    // /hello?name=Java
     @GetMapping("/hello")
     public String mainWithParam(
             @RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) {
 
         model.addAttribute("message", name);
-
+        Client client = new Client();
+        model.addAttribute("client", client);
+        model.addAttribute("listAction", listAction);
+        List<Client> userList = getClients();
+        model.addAttribute("userList", userList);
         return "welcome"; //view
     }
 
---
+++ frontend/src/main/java/com/rhdhv/frontend/entity/Client.java
@@ -0,0 +1,35 @@
+package com.rhdhv.frontend.entity;
+public class Client {
+    /*Class used to mapping data clients (name and ID)
+    * To the WelcomeController.java and welcome.html in somehow .... */
+
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    private String action;
+}
