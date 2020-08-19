9ae1caa0318591e0c5ee4cb9f3d34372ef5ac742
Bernardo Garcia <bernardo.garcia.loaiza@sentia.com>
2020-08-19 02:47:12 +0200

    Testing ./mvnw spring-boot:build-image command on github actions

--- frontend/src/main/java/com/rhdhv/frontend/controller/WelcomeController.java
+++ frontend/src/main/java/com/rhdhv/frontend/controller/WelcomeController.java
@@ -3,6 +3,7 @@ package com.rhdhv.frontend.controller;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rhdhv.frontend.entity.Client;
+import com.rhdhv.frontend.util.Constants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -36,7 +37,7 @@ public class WelcomeController {
         return "welcome"; //view
     }
 
-    // /hello?name=Java
+    // /hello?name=Java just a test to get a hello message on template
     @GetMapping("/hello")
     public String mainWithParam(
             @RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) {
@@ -50,23 +51,25 @@ public class WelcomeController {
         return "welcome"; //view
     }
 
+
     @PostMapping("/backend")
-    public String submitForm(@ModelAttribute("client") Client client,Model model) {
-        System.out.println("user.getName():"+client.getName());
-        System.out.println("user.getAction:"+client.getAction());
+    public String submitForm(@ModelAttribute("client") Client client, Model model) {
+        System.out.println("user.getName():" + client.getName());
+        System.out.println("user.getAction:" + client.getAction());
         model.addAttribute("message", "Calling the Backend");
         model.addAttribute("client", client);
         model.addAttribute("listAction", listAction);
+
+        if ((client.getAction() != null) && (client.getName().trim().length() > 0)) executeAccion(client, model);
+
         List<Client> userList = getClients();
         model.addAttribute("userList", userList);
-
         return "welcome";
     }
 
     /* Retrieving clients */
-    private static List<Client>  getClients()
-    {
-        final String uri = "http://localhost:8080/clients";
+    private static List<Client> getClients() {
+        final String uri = "https://backend.bgarcial.me/clients";
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
         System.out.println(result);
@@ -76,11 +79,29 @@ public class WelcomeController {
             });
             System.out.println(participantJsonList.size());
             return participantJsonList;
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return new ArrayList<Client>();
     }
 
+    /* Adding and delete clients */
+    private void executeAccion(Client client, Model model) {
 
+        if (Constants.ADD.equals(client.getAction())) {
+            final String uri = "https://backend.bgarcial.me/clients";
+            RestTemplate restTemplate = new RestTemplate();
+            Client result = restTemplate.postForObject(uri, client, Client.class);
+            System.out.println("Run Name:" + result.getName());
+            System.out.println("Run Id:" + result.getId());
+
+            model.addAttribute("message", "Calling the Backend - User was Added");
+
+        } else if (Constants.Delete.equals(client.getAction())) {
+            final String uri = "https://backend.bgarcial.me/clients/delete";
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.postForObject(uri, client, ArrayList.class);
+            model.addAttribute("message", "Calling the Backend - User was Removed");
+        }
+    }
 }
