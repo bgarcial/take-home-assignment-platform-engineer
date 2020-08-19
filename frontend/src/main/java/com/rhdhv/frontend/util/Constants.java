e14aac055648b79b677f29feb95a72f942292e4e
Bernardo Garcia <bernardo.garcia.loaiza@sentia.com>
2020-08-19 05:08:33 +0200

    Creating Constants for choose actions on UI [add or delete]

--- frontend/src/main/java/com/rhdhv/frontend/util/Constants.java
+++ frontend/src/main/java/com/rhdhv/frontend/util/Constants.java
@@ -1,5 +1,6 @@
 package com.rhdhv.frontend.util;
 
+/* Constants for actions to delete and add customers on UI*/
 public class Constants {
     public static final String ADD ="Add";
     public static final String Delete ="Delete";
